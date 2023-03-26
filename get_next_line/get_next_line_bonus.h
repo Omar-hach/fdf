af82807fab9e90d8d7a43ed240e73a325f98b2ef
Omar-hach <114673874+Omar-hach@users.noreply.github.com>
2023-03-26 22:24:12 +0000

    commiting utils functions

---
+++ get_next_line/get_next_line_bonus.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ohachami <ohachami@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/12/04 01:28:55 by ohachami          #+#    #+#             */
+/*   Updated: 2022/12/04 01:28:58 by ohachami         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include<unistd.h>
+# include<stdlib.h>
+# include<fcntl.h>
+# include<limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 2000
+# endif
+
+void	ft_bzero_b(void *s, size_t n);
+void	*ft_calloc_b(size_t count, size_t size);
+char	*get_next_line(int fd);
+char	*ft_strjoin_b(char *s1, char *s2);
+int		ft_strlen(const char *str);
+
+#endif
